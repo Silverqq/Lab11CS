4e6f84595c9a97a2486af248eb13f62dafa7df64
Shor <Shor@KOMPUTER>
2024-05-14 15:40:47 +0300

    Добавьте файлы проекта.

---
+++ Lab11CS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    delegate string GetNextDayDelegate();
+
+    static void Main()
+    {
+        string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int currentDayIndex = 0;
+
+        GetNextDayDelegate getNextDay = () =>
+        {
+            string nextDay = daysOfWeek[currentDayIndex];
+            currentDayIndex = (currentDayIndex + 1) % daysOfWeek.Length;
+            return nextDay;
+        };
+
+        for (int i = 0; i < 7; i++) // Продемонстрируем вызов делегата несколько раз
+        {
+            string nextDay = getNextDay();
+            Console.WriteLine(nextDay);
+        }
+    }
+}
